917d57dc580d0586e9771641cec3f7b20b9b736e
Kim SangHo <x2hokim@daum.net>
2018-10-24 13:43:12 +0900

    final version upload

--- MCCToken.sol
+++ MCCToken.sol
@@ -12,6 +12,8 @@ contract MCCToken is TransferLimitedToken {
 
     uint8 public decimals = 18;
 
+    event Burn(address indexed burner, uint256 value);
+
     // =================================================================================================================
     //                                         Constructor
     // =================================================================================================================
@@ -26,7 +28,8 @@ contract MCCToken is TransferLimitedToken {
         TransferLimitedToken(_listener, _owners, _manager)
     {
         totalSupply_ = uint256(1000000000).mul(uint256(10) ** decimals); // token total supply : 1000000000
-        balances[msg.sender] = totalSupply_;
+
+        balances[_owners[0]] = totalSupply_;
     }
 
     /**
@@ -43,12 +46,35 @@ contract MCCToken is TransferLimitedToken {
         emit Transfer(msg.sender, _to, _value);
     }
 
-    function burn(uint256 _amount) external onlyOwner
-    {
-        require(_amount <= balances[msg.sender]);
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        _burn(msg.sender, _value);
+    }
 
-        balances[msg.sender] = SafeMath.sub(balances[msg.sender], _amount);
-        balances[address(0)] = SafeMath.add(balances[address(0)], _amount);
-        emit Transfer(msg.sender, address(0), _amount);
+    /**
+    * @dev Burns a specific amount of tokens from the target address and decrements allowance
+    * @param _from address The address which you want to send tokens from
+    * @param _value uint256 The amount of token to be burned
+    */
+    function burnFrom(address _from, uint256 _value) public {
+        require(_value <= allowed[_from][msg.sender]);
+        // Should https://github.com/OpenZeppelin/zeppelin-solidity/issues/707 be accepted,
+        // this function needs to emit an event with the updated approval.
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        _burn(_from, _value);
+    }
+
+    function _burn(address _who, uint256 _value) internal {
+        require(_value <= balances[_who]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        balances[_who] = balances[_who].sub(_value);
+        totalSupply_ = totalSupply_.sub(_value);
+        emit Burn(_who, _value);
+        emit Transfer(_who, address(0), _value);
     }
 }
